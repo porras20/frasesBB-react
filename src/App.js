f96c8042f37e6bc5229bb8c4eb69ec5193944a08
Juan Jose Porras <juanjoseporras2019@gmail.com>
2023-02-24 11:33:57 -0500

    Proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,58 @@
+import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
+import { Frase } from "./components/Frase";
+
+const Contenedor = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+padding-top: 5rem;
+flex-direction: column;
+`
+
+const Boton = styled.button`
+background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+background-size: 300px;
+font-family: Arial, Helvetica, sans-serif;
+color: #fff;
+margin-top: 3rem;
+padding: 1rem 3rem;
+font-size: 2rem;
+border: 2px solid black;
+transition: background-size .8s ease;
+:hover{
+  cursor: pointer;
+  background-size: 400;
+}
+`
 function App() {
+
+  // state de frase
+  const [frase, setFrase] = useState({})
+
+  const consultarAPI = async () =>{
+    const api = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const frase = await api.json();
+    setFrase(frase[0])
+  }
+
+  //Cargar frase 
+
+  useEffect(() => {
+    consultarAPI()
+  }, [])
+  
   return (
-    <h1>Breaking bad</h1>
+    <Contenedor>
+      <Frase
+        frase={frase}
+      ></Frase>
+      <Boton
+      onClick={consultarAPI}
+      >
+      Obtener frase
+      </Boton>
+    </Contenedor>
   );
 }
 
---
+++ src/components/Frase.jsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled"
+
+const ContenedorFrase = styled.div`
+padding: 2rem;
+border-radius: .5rem;
+background-color: #ffffffd3;
+max-width: 800px;
+max-height: 300px;
+/* @media (min-width: 992px) {
+    margin-top: 10rem;
+} */
+
+h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    position: relative;
+    font-size: 2rem;
+    
+    &::before{
+        content: open-quote;
+        font-size: 5rem;
+        color: black;
+        position: relative;
+        left: -1rem;
+        top: -2rem;
+    }
+}
+p{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 1.4rem;
+    font-weight: bold;
+    text-align: right;
+    color: #666;
+    margin-top: 2rem;
+}
+`
+
+export const Frase = ({frase}) =>{
+    // if (Object.key(frase).length === 0) {
+    //     return null
+    // }
+    return (
+        <ContenedorFrase>
+            <h1>{frase.quote}</h1>
+            <p>{frase.author}</p>
+        </ContenedorFrase>
+    )
+}
